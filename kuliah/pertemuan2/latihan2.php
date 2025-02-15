6e5ed6c8cf6296a77d6c31ecab99e0df3ea61dca
nailazalfaa <znaila521@gmail.com>
2025-02-15 23:18:42 +0700

    selesai

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+    $nama = "Naila Zalfa Winarti";
+    $email = "nailazalfa79@gmail.com";
+    $pekerjaan = "Mahasiswa";
+    $jurusan = "Teknik Informatika";
+    $universitas = "Universitas Pasundan";
+
+    echo "<h1>$nama</h1>";
+    echo "<h4>$email</h4>";
+    echo "<p>Halo, nama saya $nama saya seorang $pekerjaan di $jurusan $universitas</p>";
+
+?>
